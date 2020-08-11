4dc2d3d1340d8f2268904b48df9a76bdfff92066
Leonardo Testa <loscabres@hotmail.com>
2020-08-10 20:29:17 -0400

    20 C#: Linq Métodos (Parte 2)- Skip, Take, SkipWhile, TakeWhile, Concat

---
+++ Clase17/Clase17/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Clase17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Numero1 = new[] { 1, 2, 2, 2, 3, 3, 4, 5, 6, 5, 6, 5, 4, 2, 1, 2, 8, 9, 5 };
+            int[] Numero2 = new[] { 8, 2, 2, 5, 3, 4 };
+            int[] Numero3 = new[] { 5, 6, 5, 6, 5, 4, 2, 1, 2, 8, 9, 5 };
+
+            //SKIP (SALTAR)
+            //List<int> Datos = Numero1.Skip(7).ToList();
+            //Datos.ForEach(n => Console.Write($"{n}, "));
+
+            //TAKE (OBTENER O TOMAR)
+            //List<int> Datos = Numero1.Take(5).ToList();
+            //Datos.ForEach(n => Console.Write($"{n}, "));
+
+            //SKIP CON TAKE
+            //List<int> Datos = Numero1.Skip(7).Take(6).ToList();
+            //Datos.ForEach(n => Console.Write($"{n}, "));
+
+            //SKIPWHILE (SALTAR MIENTRAS)
+            //List<int> Datos = Numero1.SkipWhile(n => n < 5).ToList();
+            //Datos.ForEach(n => Console.Write($"{n}, "));
+
+            //TAKEWHILE(OBTENER MIENTRAS)
+            //List<int> Datos = Numero1.TakeWhile(n => n < 5).ToList();
+            //Datos.ForEach(n => Console.Write($"{n}, "));
+
+            //SKIPWHILE CON TAKEWHILE
+            //List<int> Datos = Numero1.SkipWhile(n => n < 5).TakeWhile(n => n < 8).ToList();
+            //Datos.ForEach(n => Console.Write($"{n}, "));
+
+            //CONCAT (CONCATENAR)
+            //List<int> Datos = Numero1.Concat(Numero2).ToList();
+            //Datos.ForEach(n => Console.Write($"{n}, "));
+
+        }
+    }
+}
